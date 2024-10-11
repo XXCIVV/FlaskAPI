073723bcf4988269a4147431526f6f546e526797
Tomon <tomon.saks@bumail.net>
2024-10-11 07:16:53 +0700

    Update 4

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
+@app.route('/', methods=['GET', 'POST'])
+def index(): 
+    return "BU PROJECT MDT TESTING MESSAGE"
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload_file():
@@ -29,6 +31,6 @@ def upload_file():
     return jsonify({"message": "File uploaded successfully", "file_path": file_path}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
     
